eee5479b15d74d5ceeafb3e5d556b6808a4352e8
Eddie Barzi <73700112+19eddie@users.noreply.github.com>
2022-03-22 11:30:59 +0100

    removed useless "new"

--- src/main/scala/u04lab/code/Complex.scala
+++ src/main/scala/u04lab/code/Complex.scala
@@ -7,8 +7,8 @@ trait Complex:
   def *(c: Complex): Complex // should implement the product of two complex numbers
 
 object Complex:
-  def apply(re: Double, im: Double): Complex =
-      new ComplexImpl(re, im)
+  def apply(re: Double, im: Double): Complex = 
+    ComplexImpl(re, im)
   private case class ComplexImpl(override val re: Double,
                             override val im: Double) extends Complex:
     override def +(c: Complex): Complex = Complex(c.re + re, c.im + im)
